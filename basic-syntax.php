212891a4068c1d701a7932bd1efc0277f10b81cb
nerdthejohn <newtonjohn043@gmail.com>
2020-10-17 17:50:00 +0100

    committe

---
+++ basic-syntax.php
@@ -0,0 +1,24 @@
+<?php
+
+    //==== First Major Task
+
+    /*
+        Occupation: Student
+        Gender: Male
+        SideHustle Username: nerdthejohn
+    */
+
+
+    //About Me
+    echo '<center> <h1>ABOUT ME</h1> </center>'
+
+    //SideHustle Internship Username
+    echo '<h3>nerdthejohn</h3>'
+
+    //About Myself
+    echo "<p>I am a Statistics student in the University of Lagos.<br>
+            I am a creative individual with skills i software development.
+            My hobbies are reading books, coding and travelling.</p>"
+    
+?>
+
---
+++ major-task-1.php
@@ -0,0 +1,46 @@
+<?php
+
+    //========Declare the variables here==========
+    $var1 = 100;
+    $var2 = 2;
+
+    /*
+        Major Task for Week 1 Side Hustle Internship
+    */
+
+    //Addition
+    echo '<h1>Addition</h1>';
+    echo '<p>This program adds two variables together</p>';
+
+    echo $var1 . " + " . $var2 . " = " . ($var1 + $var2);
+
+    //Subtraction
+    echo '<h1>Subtraction</h1>';
+    echo '<p>This program subtracts two variables from each other</p>';
+
+    echo $var1 . " - " . $var2 . " = " . ($var1 - $var2);
+
+
+    //Multiplication
+    echo '<h1>Multiplication</h1>';
+    echo '<p>This program multiplies two variables together</p>';
+
+    echo $var1 . " * " . $var2 . " = " . ($var1 * $var2);
+
+
+    //Division
+    echo '<h1>Division</h1>';
+    echo '<p>This program divides two variables</p>';
+
+    echo $var1 . " / " . $var2 . " = " . ($var1 / $var2);
+
+
+    //Modulus
+    echo '<h1>Modulus</h1>';
+    echo '<p>This program divides two variables and return their remainder if there is any.</p>';
+
+    echo $var1 . " % " . $var2 . " = " . ($var1 % $var2);
+
+
+    
+?>
